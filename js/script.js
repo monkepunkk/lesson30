fe7b325ad427973419710b5901cb5f60fa329b16
Alexey Gusev <monkepunkk@gmail.com>
2022-08-30 14:09:19 +0300

    30.08

--- js/script.js
+++ js/script.js
@@ -23,21 +23,49 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
-
+/* movieDB.movies[movieDB.movies.length] = prompt  */('vvedi film','');
 const promo__adv = document.querySelector('.promo__adv');
 promo__adv.remove();
 const films_name = document.querySelectorAll('.promo__interactive-item');
 const genre = document.querySelector('.promo__genre'),
       promo = document.querySelector('.promo__bg'),
-      movieList = document.querySelector('.promo__interactive-list');
+      movieList = document.querySelector('.promo__interactive-list'),
+      addForm = document.querySelector('form.add'),
+      addInput = addForm.querySelector ('.adding__input'),
+      checkbox = addForm.querySelector('[type="checkbox"]');
+
+
+addForm.addEventListener('submit',(event)=> {
+    event.preventDefault();
+    let newFilm = addInput.value,
+          favorite = checkbox.checked;
+    if (newFilm){
+        if (newFilm.length > 21 ){
+            newFilm = `${newFilm.substring(0,20)}...`;
+        }
+        movieDB.movies.push(newFilm);
+        movieDB.movies.sort();
+        createMovieList(movieDB.movies,movieList);
+    }
+});
+const deleteAdv = (arr) => {
+    arr.forEach(item => {
+        item.remove();
+    }); 
+};
 genre.textContent = 'drama';
 promo.style.backgroundImage = 'url("img/bg.jpg")';
-movieList.innerHTML = "";
-movieDB.movies.sort();
-movieDB.movies.forEach((film,i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i+1} ${film} 
-        <div class="delete"></div>
-    </li>
-    `;
-});
+function createMovieList (films, parent){
+    parent.innerHTML = "";
+    films.forEach((film,i) => {
+        parent.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${film} 
+            <div class="delete"></div>
+        </li>
+        `;
+    });
+}
+
+
+
+
